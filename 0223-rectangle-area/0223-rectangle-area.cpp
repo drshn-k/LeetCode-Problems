85477bfdb1bb24f77061ce2ce5b88bc5c4d03776
drshn-k <110783201+drshn-k@users.noreply.github.com>
2022-11-17 12:45:29 +0530

    Time: 8 ms (86.37%), Space: 5.9 MB (75.43%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2-ax1) * (ay2-ay1);
+        int area2 = (bx2-bx1) * (by2-by1);
+        int comarea = 0;
+        
+        int left = max(ax1, bx1);
+        int right = min(ax2, bx2);
+        int top = min(ay2, by2);
+        int bottom = max(ay1, by1);
+        
+        if(left < right && top > bottom)
+            comarea = (right-left) *(top-bottom);
+            
+        return area1 + area2 - comarea;
+    }
+};
