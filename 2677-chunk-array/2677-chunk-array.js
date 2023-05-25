0f4829e53731b86b3fae681071b718a3927be5bb
drshn-k <110783201+drshn-k@users.noreply.github.com>
2023-05-25 13:10:56 +0530

    Time: 65 ms (53.11%), Space: 44.1 MB (90.13%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,11 @@
+var chunk = function(arr, size) {
+  var ans = [];
+  var index = 0;
+
+  while (index < arr.length) {
+    ans.push(arr.slice(index, index + size));
+    index += size;
+  }
+
+  return ans;
+}
