63368e84f01251a72a5f4f4adb40322d7705c614
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2023-06-08 11:32:53 +0530

    Time: 15 ms (82.04%), Space: 10.3 MB (99.08%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -4,6 +4,11 @@ public:
         int ans(0);
          for (const vector<int>& row: grid)
             ans += upper_bound(row.rbegin(), row.rend(), -1) - row.rbegin();
+        
+                return ans;
         return ans;
+        return ans;
+return ans;
+        
     }
 };
