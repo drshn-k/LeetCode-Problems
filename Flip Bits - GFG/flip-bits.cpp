ec6505ffe5c283588f2132fba0f62c9c24ba73a7
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2023-08-15 11:37:51 +0530

    Added solution - LeetHub

---
+++ Flip Bits - GFG/flip-bits.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    int maxOnes(int a[], int n)
+    {  
+        // Your code goes here
+        int sum=0;
+        int maxi=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==0)
+            {
+                ++sum;
+                maxi=max(sum,maxi);
+            }
+            if(a[i]==1)
+            {
+                --sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            maxi+=a[i];
+        }
+        return maxi;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n+5];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<< ob.maxOnes(a, n) <<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
