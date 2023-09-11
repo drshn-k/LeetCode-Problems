dd8c39764c8de9e3ab96d1697f318010d48bc550
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2023-09-11 19:06:03 +0530

    Time: 10 ms (64.93%), Space: 13.9 MB (17.00%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& g) {
+         
+        vector <vector<int>> ans;
+
+        vector <pair<int,int>> v;
+
+        for ( int i=0; i<g.size(); i++ ) {
+            v.push_back({g[i],i});
+        }
+
+        sort(v.begin(),v.end());
+
+        int k = 0;
+
+        for ( int i=0; i<v.size(); ) {
+
+            vector <int> temp;
+
+            for ( int j=0; j<v[i].first; j++ ) {
+                temp.push_back(v[k++].second);
+            }
+
+            i = k;
+
+            ans.push_back(temp);
+        }
+
+        return ans;   
+    }
+};
