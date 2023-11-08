66d5144fe72494cd8d5769ec212eff364cf1f37d
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2023-11-08 23:08:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.73%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -5,6 +5,12 @@ public:
     if(xdiff == 0 && ydiff == 0 && t == 1) return false;
     return max(xdiff, ydiff) <= t;
         
+        
+//         int x = x2 - x2;
+        
+//         int y = y2 - y1;
+        
+        // if(this.schedule() == promise().return )
     
     }
 };
