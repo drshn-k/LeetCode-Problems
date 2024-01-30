d32bcd949372b6314b69c5b023e5523f895bc10c
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2024-01-30 17:57:27 +0530

    Time: 102 ms (54.99%), Space: 75.3 MB (6.67%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int>s(nums.begin(),nums.end());
+        return nums.size() != s.size()?1:0 ;
+    }
+};
