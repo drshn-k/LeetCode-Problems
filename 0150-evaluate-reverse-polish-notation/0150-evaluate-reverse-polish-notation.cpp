b4128f42e70a47f40bafa5de2dd77ba688a47530
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2024-01-30 10:44:14 +0530

    Time: 3 ms (98.68%), Space: 15.5 MB (5.26%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
-        
-        stack<int> st;
-        
-        for(auto& i: tokens){
-            // cout << i << << 
-            // cout << typeid(i).name() <<endl;
-        if(i == "+" or i == "-" or i == "*" or i == "/"){
-               int op1 = st.top();
-                st.pop();
-                int op2 = st.top();
-                st.pop();
-         //        char ch = i[0];
-         // switch(ch) {
-         //        case "+": x1=x2 + x1; break;
-         //        case "-": x1 =x2 - x1; break;
-         //        case "*": x1 =x2 * x1; break;
-         //        case "/": x1 = x2/x1; break;
-         //    }
-            // if(i == "=\")
-            if(i == "+") op1 = op2 + op1;
-			if(i == "-") op1 = op2 - op1;
-			if(i == "/") op1 = op2 / op1;
-			if(i == "*") op1 = op2 * op1; 
-                st.push(op1);
-
-        }
-            
-            else{
-            st.push(stoi(i));
-                             
-            }
-            
-            cout << i << " " << st.top() << endl;
-        }
-        
-        return st.top();
-        
-    }
-    
 //     int evalRPN(vector<string>& tokens) {
-// 	stack<int> s;
-// 	for(auto& t : tokens) 
-// 		if(t == "+" || t == "-" || t == "*" || t == "/") {
-// 			int op1 = s.top(); s.pop();
-// 			int op2 = s.top(); s.pop();
-// 			if(t == "+") op1 = op2 + op1;
-// 			if(t == "-") op1 = op2 - op1;
-// 			if(t == "/") op1 = op2 / op1;
-// 			if(t == "*") op1 = op2 * op1;   
-// 			s.push(op1);
-// 		}
-// 		else s.push(stoi(t));    // stoi - converts from string to int     
-// 	return s.top(); 
-// }
+        
+//         stack<int> st;
+        
+//         for(auto& i: tokens){
+//             // cout << i << << 
+//             // cout << typeid(i).name() <<endl;
+//         if(i == "+" or i == "-" or i == "*" or i == "/"){
+//                int op1 = st.top();
+//                 st.pop();
+//                 int op2 = st.top();
+//                 st.pop();
+//          //        char ch = i[0];
+//          // switch(ch) {
+//          //        case "+": x1=x2 + x1; break;
+//          //        case "-": x1 =x2 - x1; break;
+//          //        case "*": x1 =x2 * x1; break;
+//          //        case "/": x1 = x2/x1; break;
+//          //    }
+//             // if(i == "=\")
+//             if(i == "+") op1 = op2 + op1;
+// 			if(i == "-") op1 = op2 - op1;
+// 			if(i == "/") op1 = op2 / op1;
+// 			if(i == "*") op1 = op2 * op1; 
+//                 st.push(op1);
+
+//         }
+            
+//             else{
+//             st.push(stoi(i));
+                             
+//             }
+            
+//             cout << i << " " << st.top() << endl;
+//         }
+        
+//         return st.top();
+        
+//     }
+    
+    int evalRPN(vector<string>& tokens) {
+	stack<int> s;
+	for(auto& t : tokens) 
+		if(t == "+" || t == "-" || t == "*" || t == "/") {
+			int op1 = s.top(); s.pop();
+			int op2 = s.top(); s.pop();
+			if(t == "+") op1 = op2 + op1;
+			if(t == "-") op1 = op2 - op1;
+			if(t == "/") op1 = op2 / op1;
+			if(t == "*") op1 = op2 * op1;   
+			s.push(op1);
+		}
+		else s.push(stoi(t));    // stoi - converts from string to int     
+	return s.top(); 
+}
 };
