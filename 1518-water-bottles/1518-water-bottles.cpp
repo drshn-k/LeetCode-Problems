7681e36b9f244af5634ca7e4f53fa2b994709253
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2024-07-07 20:49:22 +0530

    Time: 3 ms (13.78%), Space: 7.2 MB (30.43%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+           int sum=numBottles;
+        while(numBottles>=numExchange){
+            sum=sum+numBottles/numExchange;
+            numBottles=numBottles/numExchange+numBottles%numExchange;
+        }
+        return sum;
+    }
+};
